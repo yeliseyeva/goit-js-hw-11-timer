1cf4dc883ee98062a3e70313b983366c12326ec3
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-08-21 18:29:22 +0300

    done

--- timer.js
+++ timer.js
@@ -2,7 +2,7 @@ const refs = {
     day: document.querySelector('[data-value="days"]'),
     hour: document.querySelector('[data-value="hours"]'),
     min: document.querySelector('[data-value="mins"]'),
-    sec: document.querySelector('[data-value="secs"]')
+    sec: document.querySelector('[data-value="secs"]'),
 };
 
 const timer = {
